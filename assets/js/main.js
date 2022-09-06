3988715b196e85d9142ebd8da3eab414fed9e5d4
NTAILERATO <112897646+NTAILERATO@users.noreply.github.com>
2022-09-06 16:09:31 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,40 @@
 
 
         function tabs(tb)
-        {
+        { 
+          if(tb == "tb2")
+          {
+            var a = readCookie("QRCODE");
+
+            $(".tbs").css("display" , "none");
+
+            if(a == null)
+            {
+              load();
+              $("#" + tb).css("display" , "block");
+            }
+            else
+            {
+               $("#tb1").css("display" , "block");
+            }
+          }
+          else{
             $(".tbs").css("display" , "none");
             $("#" + tb).css("display" , "block");
+          }
 
         }
+
+        
+        function createCookie(name, value, days) 
+        {
+          localStorage.setItem(name, value); 
+       }
+
+       function readCookie(name) 
+       {
+        return localStorage.getItem(name);
+       }
 
         var chartT = new Highcharts.chart('container', {
             chart: {
